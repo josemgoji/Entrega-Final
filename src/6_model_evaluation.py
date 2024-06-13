8ccd7f755ac9dcb0ae8bba05c425bff8bb04a339
jmgomezj1 <jmgomezj1@eafit.edu.co>
2024-06-13 12:30:58 -0500

    :hammer: creacion nuevo paso del pipeline y actualizacion modelos

--- src/6_model_evaluation.py
+++ src/6_model_evaluation.py
@@ -1,4 +1,3 @@
-
 # importar librerias de python
 import os
 import sys
@@ -7,17 +6,10 @@ import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import cross_validate , StratifiedKFold
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import ConfusionMatrixDisplay , precision_recall_curve , RocCurveDisplay
 from sklearn.metrics import accuracy_score,roc_auc_score, precision_score, recall_score, f1_score, average_precision_score
-from sklearn.model_selection import StratifiedKFold
-from imblearn.over_sampling import RandomOverSampler
 import joblib
-
 
 current_dir = os.getcwd() # Obtener la ruta del directorio actual del notebook
 ROOT_PATH = os.path.dirname(current_dir) # Obtener la ruta del directorio superior
@@ -25,91 +17,6 @@ sys.path.insert(1, ROOT_PATH) # Insertar la ruta en sys.path
 
 import root # Importar el módulo root
 
-
-# ## Modelos info users
-
-file_path = root.DIR_DATA_STAGE + 'train_infousers.csv'
-
-
-df_infousers = pd.read_csv(file_path)
-df_infousers.head()
-
-
-# ### info usuarios
-
-
-X = df_infousers.drop(columns=['Cuotas en mora'])
-X = StandardScaler().fit_transform(X)
-y = df_infousers['Cuotas en mora']
-
-
-# Inicializar los modelos
-logistic_regression = LogisticRegression(max_iter=1000)
-naive_bayes = GaussianNB()
-lda = LinearDiscriminantAnalysis()
-
-
-metricas = ['accuracy', 'precision', 'recall', 'f1','roc_auc','average_precision']
-
-
-cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-
-
-logistic_scores = cross_validate(logistic_regression, X, y, cv=cv, scoring=metricas)
-naive_bayes_scores = cross_validate(naive_bayes, X, y, cv=cv, scoring=metricas)
-lda_scores = cross_validate(lda, X, y, cv=cv, scoring=metricas)
-
-def print_results(model_name, scores):
-    print(f"\n{model_name}")
-    for metrica in metricas:
-        mean_score = np.mean(scores[f'test_{metrica}'])
-        std_score = np.std(scores[f'test_{metrica}'])
-        print(f"{metrica.capitalize()}: {mean_score:.2f} +/- {std_score * 2:.2f}")
-
-print_results("Regresión Logística", logistic_scores)
-print_results("Naive Bayes", naive_bayes_scores)
-print_results("LDA", lda_scores)
-
-
-
-
-
-# ## Modelos para CredInfo
-
-
-file_path = root.DIR_DATA_STAGE + 'train_creditinfo.csv'
-
-
-df_credit_info = pd.read_csv(file_path)
-df_credit_info.head()
-
-
-X = df_credit_info.drop(columns=['mora'])
-X = StandardScaler().fit_transform(X)
-y = df_credit_info['mora']
-
-
-# ## Scores usando k startify K-fold
-
-
-logistic_scores = cross_validate(logistic_regression, X, y, cv=cv, scoring=metricas)
-naive_bayes_scores = cross_validate(naive_bayes, X, y, cv=cv, scoring=metricas)
-lda_scores = cross_validate(lda, X, y, cv=cv, scoring=metricas)
-
-
-print_results("Regresión Logística", logistic_scores)
-print_results("Naive Bayes", naive_bayes_scores)
-print_results("LDA", lda_scores)
-
-
-
-# ## Modelación
-
-
-# ### Naive Bayes
-
-
-##Función pára curva lift
 
 def plot_comulative_gain(y_true, y_proba, ax):
     # Create a dataframe with true labels and predicted probabilities
@@ -156,21 +63,6 @@ def plot_precision_recall_curve(y, y_proba, ax):
     ax.set_ylabel('Precision')
     ax.legend()
 
-
-def modelo(df,target_col,model):
-    
-    X = df.drop(columns=[target_col])
-    X = StandardScaler().fit_transform(X)
-    y = df[target_col]
-    
-    ros = RandomOverSampler(random_state=0)
-    X, y = ros.fit_resample(X, y)
-    
-    model.fit(X, y)
-    
-    return model
-
-
 def graficas(df, target_col, model):
     
     scaler = StandardScaler()
@@ -203,7 +95,6 @@ def graficas(df, target_col, model):
     fig1, axs = plt.subplots(1, 1, figsize=(10, 6))
     disp = ConfusionMatrixDisplay.from_estimator(model, X, y, normalize='true', xticks_rotation='vertical', ax=axs)
     axs.set_title("Confusion Matrix")  # Add a title to the plot
-    plt.show()
     
 
     fig2, axs = plt.subplots(2, 2, figsize=(12, 10))
@@ -217,7 +108,6 @@ def graficas(df, target_col, model):
     
     fig2.subplots_adjust(wspace=0.3)
     plt.tight_layout()
-    plt.show()
     
     df = pd.DataFrame(scaler.inverse_transform(X), columns=columns)
     
@@ -227,25 +117,43 @@ def graficas(df, target_col, model):
     
     return df, fig1, fig2
 
-
+#cargar datos de test
 
 file_path2 = root.DIR_DATA_STAGE + 'test_infousers.csv'
 test_infousers = pd.read_csv(file_path2)
 
-
-
-Modelo1 = modelo(df_infousers,'Cuotas en mora',logistic_regression)
-df_final, fig1_1, fig2_1 = graficas(test_infousers, 'Cuotas en mora', model = Modelo1)
-model1_path = root.DIR_DATA_ANALYTICS + 'model1_infousers.pkl'
-joblib.dump(Modelo1, model1_path)
-
 file_path2 = root.DIR_DATA_STAGE + 'test_creditinfo.csv'
 test_credit_info = pd.read_csv(file_path2)
 
-Modelo2 = modelo(df_credit_info,'mora',logistic_regression)
+# cargar modelos
+
+Modelo1 = joblib.load(root.DIR_DATA_ANALYTICS + 'model1_infousers.pkl')
+Modelo2 = joblib.load(root.DIR_DATA_ANALYTICS + 'model2_creditinfo.pkl')
+
+
+
+
+df_final, fig1_1, fig2_1 = graficas(test_infousers, 'Cuotas en mora', model = Modelo1)
 df_final_2, fig1_2, fig2_2 = graficas(test_credit_info, 'mora', model = Modelo2)
+
+
+# guardar graficas, modelos y df final
+df_final_path = root.DIR_DATA_ANALYTICS + 'df_final_infousers.csv'
+cm_path = root.DIR_DATA_ANALYTICS + 'confusion_matrix_infousers.png'
+plots_path = root.DIR_DATA_ANALYTICS + 'graficas_infousers.png'
+model1_path = root.DIR_DATA_ANALYTICS + 'model1_infousers.pkl'
+fig1_1.savefig(cm_path)
+fig2_1.savefig(plots_path)
+df_final.to_csv(df_final_path, index=False)
+
+
+df_final2_path = root.DIR_DATA_ANALYTICS + 'df_final_creditinfo.csv'
+cm2_path = root.DIR_DATA_ANALYTICS + 'confusion_matrix_creditinfo.png'
+plots2_path = root.DIR_DATA_ANALYTICS + 'graficas_creditinfo.png'
 model2_path = root.DIR_DATA_ANALYTICS + 'model2_creditinfo.pkl'
-joblib.dump(Modelo2, model2_path)
+fig1_2.savefig(cm2_path)
+fig2_2.savefig(plots2_path)
+df_final_2.to_csv(df_final2_path, index=False)
 
 
 
